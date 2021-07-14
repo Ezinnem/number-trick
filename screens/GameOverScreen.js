eed8a9cb264fe704b5558201074bd8c4ce33e660
Ezinne Nnamani <nnamani.ezinne@gmail.com>
2021-07-14 22:49:45 +0100

    Created the Card component for the re-rendering of the Data using Card design

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const GameOverScreen = props => {
+    <View style={styles.screen}>
+        <Text>
+            The Game is Over!
+        </Text>
+        <
+    </View>
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default GameOverScreen;
